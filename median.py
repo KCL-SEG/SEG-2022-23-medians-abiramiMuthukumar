26f422697e4f04e4a1028c40ce7751d53b9113cc
Abirami Muthukumar <abimuthukumar10@gmail.com>
2023-09-29 13:38:01 +0100

    Add median functionality

--- median.py
+++ median.py
@@ -9,4 +9,12 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+
+if len(numbers) % 2 != 0:
+    print(numbers[int((len(numbers) -1)/2)])
+else:
+    num_index = int(len(numbers)/2 - 1)
+    result = (numbers[num_index] + numbers[num_index + 1]) / 2
+    print(result)
